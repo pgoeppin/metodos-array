6b5f0c5e3098c3eefd9949975e9f8ee705be4d09
Pablo Goeppinger <pgoeppin@gmail.com>
2022-07-30 22:50:22 -0400

    Adding AddBtn and renderlist

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+/* DEFININIENDO DOMS Y LISTA DE TAREAS QUE VENDRIA POR DEFECTO*/
+const tasks = [{id: 1, name: "Ir al supermercado", completed: false},
+            {id:2, name: "Pasear al perro", completed: false},
+            {id:3, name: "Ir al gimnasio", completed: false}]
+const taskList = document.querySelector("#task-list")
+const btnAdd = document.querySelector("#btn-add")
+const taskInput = document.querySelector("#task-input")
+const totalTasks = document.querySelector("#total-tasks")
+const doneTasks = document.querySelector("#done-tasks")
+function renderTasks () {
+    let html = ""
+    for (const task of tasks) {
+        html += `<tr>
+                            <td>${task.id}</td>
+                            <td>${task.name}</td>
+                            <td><input type="checkbox" id="${task.id}" name="ready" class="ready"></td>
+                            <td><button class="btn-delete" id="${task.id}">X</button></td>
+                        </tr>`;
+    };
+    taskList.innerHTML = html;
+};
+renderTasks();
+btnAdd.addEventListener("click", () => {
+    if(taskInput.value == ''){
+        alert("Ingrese una tarea, por favor.")
+    } else if (tasks.length != 0) {
+        /* Para hacer un id autoincrementable */
+        const lastIdTask = tasks[tasks.length-1].id
+        const newTask = {id: lastIdTask+1, name: taskInput.value, completed: false}
+        tasks.push(newTask)
+        taskInput.value = ""
+        renderTasks();
+    } else {
+        /* Si no hay datos, comienza con id: 1 */
+        const newTask = {id: 1, name: taskInput.value, completed: false}
+        tasks.push(newTask)
+        taskInput.value = ""
+        renderTasks();}
+});
